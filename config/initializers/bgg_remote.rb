8ba4665be2fdf289f5a7e449066edceb2c54349f
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-01-11 11:08:34 -0300

    Always configure BggRemote and default token

--- config/initializers/bgg_remote.rb
+++ config/initializers/bgg_remote.rb
@@ -1,7 +1,5 @@
-return if ENV["SECRET_KEY_BASE_DUMMY"].present?
-
 Rails.application.config.after_initialize do
   BggRemote.configure do |config|
-    config.token = Rails.application.credentials.dig(:bgg, :api_token)
+    config.token = Rails.application.credentials.dig(:bgg, :api_token).presence || ""
   end
 end

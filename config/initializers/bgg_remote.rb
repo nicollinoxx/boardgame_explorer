60e626049d0b143c8951eb3b63617a3eed1a85f8
Nícolas Amaral <64704101+nicollinoxx@users.noreply.github.com>
2026-01-10 21:08:00 -0300

    Initialize BggRemote from encrypted credentials

--- config/initializers/bgg_remote.rb
+++ config/initializers/bgg_remote.rb
@@ -1,3 +1,7 @@
-BggRemote.configure do |config|
-  config.token = Rails.application.credentials.dig(:bgg, :api_token).presence || ""
+return if ENV["SECRET_KEY_BASE_DUMMY"].present?
+
+Rails.application.config.after_initialize do
+  BggRemote.configure do |config|
+    config.token = Rails.application.credentials.dig(:bgg, :api_token)
+  end
 end

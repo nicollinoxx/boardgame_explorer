8bab2ced7114c570cfdc72646dd6aafad23abb9c
Nícolas Amaral <nicolasalmeida321@gmail.com>
2026-01-25 20:17:04 -0300

    Skip BGG initializer when dummy key present

--- config/initializers/bgg_remote.rb
+++ config/initializers/bgg_remote.rb
@@ -1,3 +1,5 @@
+return if ENV["SECRET_KEY_BASE_DUMMY"].present?
+
 Rails.application.config.after_initialize do
   BggRemote.configure do |config|
     config.token = Rails.application.credentials.dig(:bgg, :api_token).presence || ""
